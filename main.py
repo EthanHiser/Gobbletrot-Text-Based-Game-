d0c34f6bd568ae370bd6cab4f4f3597dba717eab
EthanHiser <112016131+EthanHiser@users.noreply.github.com>
2023-03-03 18:48:36 +0000

    Step 3 (Crated encounter_002trade)

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+inventory = ["Red Paperclip"]
 ui = None
 e_001 = None
+e_002 = None
+e_002trade = None
 def intro():
 	global ui
 	first = input(f"""-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n	Welcome to Red Paperclip\n\n A trading game where you use your red paperclip\nto get bigger and better items.\n\nYour in-game commands: "north", "south", "east",\n "west", "grab", "trade", "interact", "help"\n\n				    Have fun!\n\n	/// Press enter to continue ///\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n""")
@@ -14,29 +17,64 @@ def encounter_001():
 		if ui == "north":
 			e_001 = 1
 			encounter_002()
+		elif ui == "inventory":
+			print(f"\n{inventory}\n")
+			encounter_001()
 		else:
-			print(f"\n{ui} is either not an option or not valid right now!")
+			print(f"\n\"{ui}\" is either not an option or not valid right now!")
 			print()
 			encounter_001()
 	else:
-		ui = input(f"You are back at the parking lot next to your car.\n")
+		ui = input(f"\nYou are back at the parking lot next to your car.\n")
 		if ui == "north":
 			encounter_002()
+		elif ui == "inventory":
+			print(f"\n{inventory}\n")
+			encounter_001()
 		else:
-			print(f"\n{ui} is either not an option or not valid right now!")
+			print(f"\n\"{ui}\" is either not an option or not valid right now!")
 			print()
 			encounter_001()
 
 def encounter_002():
 	global ui
-	ui = input(f"You enter the farmer's market.\n")
+	global e_002
+	ui = input(f"\nYou enter the farmer's market. Around you there are vendors selling fresh fruits, hand-made clothing, and other knick-knacks.\n")
 	if ui == "north":
 		encounter_003()
 	elif ui == "south":
 		encounter_001()
+	elif ui == "trade":
+		encounter_002trade()
+	elif ui == "inventory":
+		print(f"inventory")
+		encounter_002()
 	else:
-		print(f"\n{ui} is either not an option or not valid right now!")
+		print(f"\n\"{ui}\" is either not an option or not valid right now!")
 		print()
 		encounter_002()
+
+def encounter_002trade():
+	global ui
+	global e_002trade
+	global inventory
+	if e_002trade != 1:
+		ui = input(f"\nYou walk towards a stand selling personilized wooden pens.\nDo you \"trade\" with the vendor?\n")
+		if ui == "trade":
+				print(f"\nYou explain your quest to the elderly man behind the stand. He looks at you with a confused face then laughs\nsaying \"Sure buddy, I'm sure a paperclip is going to bring you real far. You can have this old pen, it's nearly outta ink anyways.\"\n")
+				inventory.remove("Red Paperclip")
+				inventory.append("Wooden Pen")
+				print(f"+ Wooden Pen\n- Red Paperclip\n")
+				e_002trade = 1
+				encounter_003()
+		else:
+			print(f"\n\"{ui}\" is not valid right now, try typing \"trade\"")
+			print()
+			encounter_002trade()
+	else:
+		print(f"You walk towards the wooden pen vendor, but then remeber you've already traded with him.")
+		encounter_003()
+
 def encounter_003():
+	print("3")
 encounter_001()
